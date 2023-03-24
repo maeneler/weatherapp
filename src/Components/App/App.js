8d0b049eb8a60b8a10d15e60efae281c4258a172
maeneler <103448184+maeneler@users.noreply.github.com>
2023-03-24 11:44:55 +0100

    update fetch call
    add rounded Temp and day of Week to state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,21 +12,53 @@ class App extends React.Component {
     super(props);
     this.state = {
       weatherData: {},
+      forecastData: {}
     };
     this.search = this.search.bind(this);
   }
 
   search(searchTerm) {
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=167a9004313db26dbb400c1934df3405&units=metric`;
+    const API_URL_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=167a9004313db26dbb400c1934df3405&units=metric`;
+    const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=167a9004313db26dbb400c1934df3405&units=metric`;
 
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => this.setState({ weatherData: data }))
-      .catch((error) => console.log(error));
+    Promise.all([
+      fetch(API_URL_CURRENT),
+      fetch(API_URL_FORECAST)
+    ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => {
+      const currentWeatherData = data[0];
+      const forecastData = data[1];
+
+      const roundedTemp = Math.round(parseFloat(currentWeatherData.main.temp))
+      const updatedCurrentWeatherData = {...currentWeatherData, main: {...currentWeatherData.main, temp: roundedTemp}};
+      const updatedForecastData = {
+        ...forecastData,
+        list: forecastData.list.map((forecast) => {
+          //round the temp
+          const roundedForecastTemp = Math.round(parseFloat(forecast.main.temp));
+          //unix timestamp to day of the week
+          const date = new Date(forecast.dt * 1000);
+          const dayOfWeek = date.toLocaleDateString('en-US', {weekday: 'long'})
+          //return the new values
+          return{
+            ...forecast,
+             main: { ...forecast.main, temp: roundedForecastTemp },
+             dt: dayOfWeek
+          };
+        })
+      };
+
+
+      this.setState({weatherData: updatedCurrentWeatherData, forecastData: updatedForecastData})
+    })
+
   }
 
 
- 
+
+
+
   render() {
     return (
       <div className='page'>
@@ -35,9 +67,9 @@ class App extends React.Component {
           <SearchBar onSearch={this.search} />
           <div className="App-weather">
             <CurrentWeather
-              weatherData={this.state.weatherData}              
+              weatherData={this.state.weatherData}
               />
-            {/* <Forecast weatherData={this.state.weatherData} /> */}
+            <Forecast forecastData={this.state.forecastData} />
           </div>
         </div>
       </div>
