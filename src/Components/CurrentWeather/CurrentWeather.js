776564f399def25a42b6907be08ddf531e295ec7
maeneler <103448184+maeneler@users.noreply.github.com>
2023-03-24 11:46:20 +0100

    update waiting text

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -2,24 +2,22 @@ import React from "react";
 import './CurrentWeather.css';
 
 class CurrentWeather extends React.Component{
-
     render(){
-    
         return (
             <div className="container">
                 {
                     Object.keys(this.props.weatherData).length ? (
                     <div>
-                        <h2>Today's Weather in: {this.props.weatherData.name}, {this.props.weatherData.sys.country}</h2> 
+                        <h2>Today's Weather in: {this.props.weatherData.name}, {this.props.weatherData.sys.country}</h2>
                         <p>Temperature: {this.props.weatherData.main.temp} degrees Celsius</p>
                         <p>{this.props.weatherData.weather[0].description}</p>
                         <img src={`http://openweathermap.org/img/wn/${this.props.weatherData.weather[0].icon}@2x.png`} />
                     </div>
                     )
                     :(
-                    <p>Waiting for Location input.....</p>
+                    <p>Waiting for location input to display current weather...</p>
                     )
-                }  
+                }
             </div>
         )
     }
